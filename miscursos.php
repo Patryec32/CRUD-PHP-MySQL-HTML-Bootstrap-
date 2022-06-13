7cfb521c42543adbe8c7d20bdcf416da82dd4fc3
patryec <patryec30@gmail.com>
2022-06-13 01:16:38 -0300

    creacion de miscursos.html en pages y miscursos.php en inicio

--- miscursos.php
+++ miscursos.php
@@ -2,6 +2,6 @@
     require_once("template/cabecera.php"); 
     require_once("template/nav.php");
     require_once("template/main.php");
-    require_once("pages/inscripcion.html");
+    require_once("pages/miscursos.html");
     require_once("template/pie.php"); 
 ?>
