2a0b8620056ecc32444dcb0a8d9b6b5374a08ae0
patryec <patryec30@gmail.com>
2022-06-13 01:09:24 -0300

    creacion de inscripcion.html en pages y inscripcion.php en inicio

---
+++ inscripcion.php
@@ -0,0 +1,6 @@
+<?php 
+    require_once("template/cabecera.php"); 
+    require_once("template/nav.php");
+    require_once("template/main.php");
+    require_once("pages/inscripcion.html");
+    require_once("template/pie.php"); ?>
